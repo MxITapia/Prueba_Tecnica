bd492fd65c02cb66870e8dfbbf61e2c7d6b4dabe
MxITapia <admin@vector.cl>
2026-01-11 20:44:36 -0300

    chore: mover frontend a /Front

--- Front/src/app/features/products/product-form.component.ts
+++ Front/src/app/features/products/product-form.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Category } from '../../core/models/product.model';
